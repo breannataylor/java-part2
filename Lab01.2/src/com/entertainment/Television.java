0eb6bd75d59dbef6f3ac227e354422f3ac867b9f
BreAnna Taylor <thebreannataylor@gmail.com>
2023-10-31 14:53:46 -0700

    Lab01.3 (equals/hashCode) complete

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -1,5 +1,7 @@
 package com.entertainment;
 
+import java.util.Objects;
+
 public class Television {
     //fields
     private String brand;
@@ -41,6 +43,35 @@ public class Television {
 
     public void setVolume(int volume) {
         this.volume = volume;
+    }
+
+    @Override
+    public int hashCode(){
+        /*
+         * This is a poorly written hash function, because it easily yields "has collisions."
+         * A hash collision is when "different" objects have the same hash code (join by coincident.)
+         *
+         *        return getBrand().length() + getVolume();
+         * Instead, we rely on Objects.has() to give us a "scientifically correct" hash function.
+         */
+        return Objects.hash(getBrand(), getVolume());
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        boolean result = false;
+
+        //proceed only if 'obj' is really referencing a Television object
+        if (obj instanceof Television){
+           //safely downcast 'obj' to more specific reference type Television
+           Television other = (Television) obj;
+
+           //business equality is defined by brand and volume being the same.
+            result = Objects.equals(this.getBrand(), other.getBrand()) &&   // null-safe check
+                    this.getVolume() == other.getVolume();                  // primitives can't be null
+        }
+
+        return result;
     }
 
     //toString
--- Lab01.2/src/com/entertainment/client/TelevisionClient.java
+++ Lab01.2/src/com/entertainment/client/TelevisionClient.java
@@ -2,25 +2,27 @@ package com.entertainment.client;
 
 import com.entertainment.Television;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TelevisionClient {
     public static void main(String[] args) {
-        //no argument object
-        Television tv1 = new Television();
-        tv1.changeChannel(9);
-        //two argument object
-        Television tv2 = new Television("Sony",45);
-
-        //printing toString
-        System.out.println(tv1);
-        System.out.println(tv2);
-        System.out.println();
-
         //similar strings to demonstrate == vs. .equals()
         Television tv3 = new Television("Toshiba", 50);
-        Television tv4 = new Television("Toshiba", 50);
+        Television tv4 = new Television("Toshiba", 65);
 
         //demonstrating == vs. .equals(): == is obviously false
-        System.out.println("tva3 == tvb" + (tv3 == tv4));
-        System.out.println("tva3.equals(tv4)" + tv3.equals(tv4));
+        System.out.println("tva3 == tv4: " + (tv3 == tv4));
+        System.out.println("tva3.equals(tv4): " + tv3.equals(tv4));
+        System.out.println();
+
+        System.out.println(tv3.hashCode());
+        System.out.println(tv4.hashCode());
+        System.out.println();
+
+        Set<Television> tvs = new HashSet<>();
+        tvs.add(tv3);
+        tvs.add(tv4); //should be rejected as a duplicate
+        System.out.println("The size of the set is: " + tvs.size());
     }
 }
